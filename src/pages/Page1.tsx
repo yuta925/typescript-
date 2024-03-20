e9bcbed3accf1a10e82ba315b0c8a88c5bcff87c
yuta925 <yutarou041@gmail.com>
2024-03-20 20:32:05 +0900

    navigate関数でページ遷移

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,13 +1,15 @@
 import { Link, useNavigate } from "react-router-dom";
 
 export const Page1 = () => {
-  const arr = ["hoge", "fuga", "piyo"];
+  // const arr = ["hoge", "fuga", "piyo"];
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Page1</h1>
-      <Link to="/page1/detail1" state={arr}>
+      <button onClick={() => navigate("/page1/detail1")}>Detail1</button>
+      {/* <Link to="/page1/detail1" state={arr}>
         Page1Detail1
-      </Link>
+      </Link> */}
       <br />
       <Link to="/page1/detail2">Page1Detail2</Link>
     </div>
