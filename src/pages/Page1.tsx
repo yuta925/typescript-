05395a66507a279b8e8e7aeb1bd7395e0e85c885
yuta925 <yutarou041@gmail.com>
2024-03-20 20:26:22 +0900

    親から子へstate受け渡し

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,10 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = ["hoge", "fuga", "piyo"];
   return (
     <div>
       <h1>Page1</h1>
-      <Link to="/page1/detail1">Page1Detail1</Link>
+      <Link to="/page1/detail1" state={arr}>
+        Page1Detail1
+      </Link>
       <br />
       <Link to="/page1/detail2">Page1Detail2</Link>
     </div>
--- src/pages/Page1Detail1.tsx
+++ src/pages/Page1Detail1.tsx
@@ -1,3 +1,19 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1Detail1 = () => {
-  return <div>Page1Detail1</div>;
+  const { state } = useLocation();
+  return (
+    <div>
+      <h1>Page1Detail1のページ</h1>
+      {state != null ? (
+        <ul>
+          {state.map((arr: any) => {
+            return <li key="arr">{arr}</li>;
+          })}
+        </ul>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
